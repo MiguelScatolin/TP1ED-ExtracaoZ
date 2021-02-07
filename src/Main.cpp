ce0ed6db5df459bd30a673269c732fc1060ce8d0
Miguel Scatolin <miguel.scatolin@gmail.com>
2021-02-07 18:00:17 -0300

    FIX: using std explicitly

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char* argv[]) {
   if(argc < 3)
     throw "Nome do arquivo de mapa ou de comandos não informado.";
 
-  ifstream mapFile(argv[1]);
-  ifstream commandFile(argv[2]);
+  std::ifstream mapFile(argv[1]);
+  std::ifstream commandFile(argv[2]);
 
   if (mapFile.is_open())
     throw  "Erro ao abrir arquivo de mapa";
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
   Map map(mapFile);
   Base base(commandFile);
 
-  base.ExecuteCommands();
+  base.ExecuteCommands(map);
 
   base.FinalReport();
 
