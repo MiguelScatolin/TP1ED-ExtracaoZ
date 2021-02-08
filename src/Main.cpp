f22f26f0679fdcb477d843f4c9667ffce63fa2f1
Miguel Scatolin <miguel.scatolin@gmail.com>
2021-02-07 22:26:54 -0300

    FIX: file is open validation

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,23 +5,29 @@
 #include "Base.h"
 
 int main(int argc, char* argv[]) {
-  if(argc < 3)
-    throw "Nome do arquivo de mapa ou de comandos não informado.";
+  try {
+    if(argc < 3)
+      throw "Nome do arquivo de mapa ou de comandos não informado.";
 
-  std::ifstream mapFile(argv[1]);
-  std::ifstream commandFile(argv[2]);
+    std::ifstream mapFile(argv[1]);
+    std::ifstream commandFile(argv[2]);
 
-  if (mapFile.is_open())
-    throw  "Erro ao abrir arquivo de mapa";
-  if (commandFile.is_open())
-    throw  "Erro ao abrir arquivo de comandos";
+    if (!mapFile.is_open())
+      throw  "Erro ao abrir arquivo de mapa";
+    if (!commandFile.is_open())
+      throw  "Erro ao abrir arquivo de comandos";
 
-  Map map(mapFile);
-  Base base(commandFile);
 
-  base.ExecuteCommands(&map);
+    Map map(mapFile);
+    Base base(commandFile);
 
-  base.FinalReport();
+    base.ExecuteCommands(&map);
 
-  return 0;
+    base.FinalReport();
+
+    return 0;
+  }
+  catch (const char* exception) {
+    std::cout << exception << std::endl;
+  }
 }
